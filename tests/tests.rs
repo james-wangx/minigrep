ca84ef016e806a226d59bf4a77a754a35a559f39
James <pineapple_cpp@163.com>
2022-03-29 21:29:22 +0800

    Update: Don't use clone() method

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,51 +2,31 @@ use std::env;
 
 use minigrep::Config;
 
-use crate::common::common_config;
-
 mod common;
 
 #[test]
 fn get_case_sensitive_by_true() {
-    let (project, query, filename) = common_config();
+    let (project, query, filename) = common::common_config();
     let case_sensitive = String::from("true");
-
-    let args = vec![
-        project.clone(),
-        query.clone(),
-        filename.clone(),
-        case_sensitive.clone(),
-    ];
+    let args = vec![project, query, filename, case_sensitive];
 
     assert_eq!(Ok(true), Config::get_case_sensitive(&args));
 }
 
 #[test]
 fn get_case_sensitive_by_false() {
-    let (project, query, filename) = common_config();
+    let (project, query, filename) = common::common_config();
     let case_sensitive = String::from("false");
-
-    let args = vec![
-        project.clone(),
-        query.clone(),
-        filename.clone(),
-        case_sensitive.clone(),
-    ];
+    let args = vec![project, query, filename, case_sensitive];
 
     assert_eq!(Ok(false), Config::get_case_sensitive(&args));
 }
 
 #[test]
 fn get_case_sensitive_by_other() {
-    let (project, query, filename) = common_config();
+    let (project, query, filename) = common::common_config();
     let case_sensitive = String::from("other");
-
-    let args = vec![
-        project.clone(),
-        query.clone(),
-        filename.clone(),
-        case_sensitive.clone(),
-    ];
+    let args = vec![project, query, filename, case_sensitive];
 
     assert_eq!(
         Err("the third argument must be true or false"),
@@ -56,11 +36,10 @@ fn get_case_sensitive_by_other() {
 
 #[test]
 fn get_case_sensitive_by_env() {
-    let (project, query, filename) = common_config();
-
-    let args = vec![project.clone(), query.clone(), filename.clone()];
-
     env::set_var("CASE_INSENSITIVE", "whatever");
+
+    let (project, query, filename) = common::common_config();
+    let args = vec![project, query, filename];
 
     assert_eq!(Ok(false), Config::get_case_sensitive(&args));
 }
